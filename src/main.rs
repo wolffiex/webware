bdae0027dd25d5939a378791aa960fedf9bb05e7
wolffiex <adam@sparko.local>
2024-01-02 21:20:36 -0800

    Combined streams

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
+use futures::{stream, StreamExt}; // Assumed 'futures' is in the dependencies
+use anyhow::Result;
+use std::pin::Pin;
+use futures::Stream;
 use axum::{
     body::Body,
+    extract::Query,
     extract::State,
     http::{header, HeaderMap, StatusCode, Uri},
     response::{IntoResponse, Response},
@@ -11,9 +16,12 @@ use axum::{
 use axum_macros::debug_handler;
 use futures::{pin_mut, TryStreamExt};
 use serde_json::Value as Json;
+use std::collections::HashMap;
 use std::fs;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use tokio_postgres::{types::ToSql, Client, Error, NoTls};
+use futures::stream::select;
 
 #[derive(Clone)]
 struct AppState {
@@ -41,7 +49,7 @@ async fn main() -> Result<(), Error> {
 
     // Set up the router and routes
     let app = Router::new()
-        .route("/", get(stream_sql_response))
+        .route("/stream", get(stream_sql_response))
         .with_state(state);
 
     // Run the application
@@ -51,27 +59,65 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
+type JsonStream = Pin<Box<dyn Stream<Item = Result<String, AxumError>> + Send>>;
+
 #[debug_handler]
-async fn stream_sql_response(State(state): State<AppState>) -> impl IntoResponse {
+async fn stream_sql_response(
+    State(state): State<AppState>,
+    Query(params): Query<HashMap<String, String>>,
+) -> impl IntoResponse {
     let client = state.client;
     // Read the SQL contents from the file.
     let sql = fs::read_to_string("sql/test.sql").expect("Unable to read the SQL file");
+    println!("PA {:?}", params.get("q"));
+    let sql_files_content = read_sql_files().unwrap();
+    println!("fff {:?}", sql_files_content);
+
+    let streams: Vec<&str> = params
+        .get("q")
+        .expect("No streams given")
+        .split(',')
+        .collect();
 
     // Execute the query and get results.
-    let params: Vec<String> = vec![];
-    let it = client.query_raw(&sql, params).await.unwrap();
+    let sql_params: Vec<String> = vec![];
+    let it = client.query_raw(&sql, sql_params).await.unwrap();
 
     // Iterate over the stream to process each row.
-    let json_stream = Box::pin(it.map_ok(|row| {
+    let json_stream :JsonStream = Box::pin(it.map_ok(|row| {
         let eventname = "stream1";
         let value: Json = row.get(0);
-        // Convert the JSON value to a string plus a newline to separate the values
-        let json_string = format!("event: {}\ndata: {}\n\n", eventname, value);
-        json_string
-    }))
-    .map_err(|e| AxumError::new(e));
+        // SSE
+        format!("event: {}\ndata: {}\n\n", eventname, value)
+    })
+    .map_err(|e| AxumError::new(e)));
 
-    let body = Body::from_stream(json_stream);
+    let (client2, connection2) =
+        tokio_postgres::connect("host=haus dbname=monitoring user=adam password=adam", NoTls)
+            .await
+            .unwrap();
+
+    // The connection object performs the actual communication with the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection2.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+    let sql2: Vec<String> = vec![];
+    let it2 = client2.query_raw(&sql, sql2).await.unwrap();
+    let js2 : JsonStream = Box::pin(it2.map_ok(|row| {
+        let eventname = "stream2";
+        let value: Json = row.get(0);
+        println!("foo {}", value);
+        // SSE
+        format!("event: {}\ndata: {}\n\n", eventname, value)
+    })
+    .map_err(|e| AxumError::new(e)));
+
+    let body = Body::from_stream(select(json_stream, js2));
+
+    // let body = Body::from_stream(json_stream);
     return (
         // set status code
         StatusCode::NOT_FOUND,
@@ -79,4 +125,22 @@ async fn stream_sql_response(State(state): State<AppState>) -> impl IntoResponse
         [("x-custom", "custom")],
         body,
     );
+}
+
+fn read_sql_files() -> Result<HashMap<String, String>> {
+    let directory = Path::new("sql");
+    assert!(directory.is_dir());
+    Ok(fs::read_dir(directory)?
+        .filter_map(|entry| {
+            entry
+                .ok()
+                .and_then(|e| e.path().is_file().then(|| e.path()))
+        })
+        .filter(|path| path.extension().and_then(|s| s.to_str()) == Some("sql"))
+        .filter_map(|path| {
+            let file_stem = path.file_stem()?.to_str()?.to_owned();
+            let content = fs::read_to_string(&path).ok()?;
+            Some((file_stem, content))
+        })
+        .collect())
 }

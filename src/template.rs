c844bd93e3bcddca51fed5040e6e25d5c90dc833
wolffiex <adam@sparko.local>
2024-01-09 17:30:00 -0800

    template part

--- src/template.rs
+++ src/template.rs
@@ -1,54 +1,89 @@
 use anyhow::Result;
 use html5gum::{HtmlString, IoReader, Tokenizer};
+use std::cell::RefCell;
+use std::collections::BTreeMap;
 use std::fmt::Write; // Import the Write trait which provides the write! macro for Strings.
 use std::fs::File;
 use std::io::{self, BufReader};
 use std::path::PathBuf;
 
-fn to_utf8(html_string: HtmlString) -> String {
-    String::from_utf8(html_string.0.clone()).unwrap()
+fn to_utf8(html_string: HtmlString) -> Result<String> {
+    Ok(String::from_utf8(html_string.0.clone())?)
 }
 
 pub struct Template {
     pub source: PathBuf,
 }
 
+pub struct StringTemplatePart {
+    content: String,
+}
+
+impl StringTemplatePart {
+    pub fn new() -> StringTemplatePart {
+        StringTemplatePart {
+            content: String::new(),
+        }
+    }
+
+    fn push_str(&mut self, html_string: HtmlString) -> Result<()> {
+        let s = to_utf8(html_string)?;
+        Ok(self.content.push_str(&s))
+    }
+
+    pub fn write_start_tag(
+        &mut self,
+        name: HtmlString,
+        attributes: BTreeMap<HtmlString, HtmlString>,
+        self_closing: bool,
+    ) -> Result<()> {
+        write!(self.content, "<{}", to_utf8(name)?)?;
+        for (tag_name, tag_value) in attributes {
+            // let tag_value_string = to_utf8(tag_value)?;
+            if tag_value.is_empty() {
+                self.push_str(tag_name)?;
+            } else {
+                write!(self.content, "{}=\"{}\"", to_utf8(tag_name)?, to_utf8(tag_value)?)?;
+            }
+        }
+        if self_closing {
+            self.content.push_str("/");
+        }
+        Ok(self.content.push_str(">"))
+    }
+
+    pub fn write_end_tag(
+        &mut self,
+        name: HtmlString,
+    ) -> Result<()> {
+        Ok(write!(self.content, "</{}>", to_utf8(name)?)?)
+    }
+
+    pub fn finalize(self) -> String {
+        return self.content;
+    }
+}
+
 impl Template {
     pub fn parse(&self) -> Result<String> {
         let file = File::open(&self.source)?;
         let reader = BufReader::new(file);
-        let mut result = String::new();
+        let mut part = StringTemplatePart::new();
         for token in Tokenizer::new(IoReader::new(reader)).flatten() {
             // println!("{:?}", token);
             match token {
                 html5gum::Token::StartTag(tag) => {
-                    write!(result, "<{}", to_utf8(tag.name))?;
-                    if tag.attributes.len() > 0 {
-                        write!(result, " ")?;
-                    }
-                    for (tag_name, tag_value) in tag.attributes {
-                        let tag_name_string = to_utf8(tag_name);
-                        let tag_value_string = to_utf8(tag_value);
-                        if tag_value_string.is_empty() {
-                            result.push_str(&tag_name_string);
-                        } else {
-                            write!(result, "{}=\"{}\"", tag_name_string, tag_value_string)?;
-                        }
-                    }
-                    if tag.self_closing {
-                        write!(result, "/")?;
-                    }
-                    write!(result, ">")?;
-                },
-                html5gum::Token::EndTag(tag) => write!(result, "</{}>", to_utf8(tag.name))?,
-                html5gum::Token::String(html_string) => result.push_str(&to_utf8(html_string)),
+                    part.write_start_tag(tag.name, tag.attributes, tag.self_closing)?
+                }
+                html5gum::Token::EndTag(tag) => part.write_end_tag(tag.name)?,
+                html5gum::Token::String(html_string) => part.push_str(html_string)?,
+                html5gum::Token::Doctype(_) => part.push_str(HtmlString("<DOCTYPE>".as_bytes().to_vec()))?,
                 html5gum::Token::Comment(_) => (),
-                html5gum::Token::Doctype(_) => write!(result, "<DOCTYPE>")?,
                 html5gum::Token::Error(err) => {
                     panic!("Error {:?}", err)
                 }
             };
         }
-        return Ok(result);
+        return Ok(part.finalize());
     }
 }

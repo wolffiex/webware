67a4b0ae4e73adc54fa5c573272a3909eb4022ea
wolffiex <adam@sparko.local>
2024-01-15 09:46:12 -0800

    Tag handling

--- src/template.rs
+++ src/template.rs
@@ -2,15 +2,15 @@
 #![allow(unused_variables)]
 #![allow(unused_imports)]
 use anyhow::Result;
-use html5gum::{HtmlString, IoReader, Tokenizer, Token, StartTag};
+use html5gum::{HtmlString, IoReader, StartTag, Token, Tokenizer};
 use std::cell::RefCell;
 use std::collections::{BTreeMap, HashMap, HashSet};
 use std::convert::TryFrom;
 use std::fmt::Write;
 use std::fs::File;
 use std::io::{self, BufReader};
-use std::path::PathBuf;
 use std::iter::Flatten;
+use std::path::PathBuf;
 
 fn to_utf8(html_string: HtmlString) -> Result<String> {
     Ok(String::from_utf8(html_string.0)?)
@@ -19,8 +19,19 @@ fn to_utf8(html_string: HtmlString) -> Result<String> {
 fn is_void_element(tag_name: &String) -> bool {
     matches!(
         tag_name.as_ref(),
-        "area" | "base" | "br" | "col" | "embed" | "hr" |
-        "img" | "input" | "link" | "meta" | "source" | "track" | "wbr"
+        "area"
+            | "base"
+            | "br"
+            | "col"
+            | "embed"
+            | "hr"
+            | "img"
+            | "input"
+            | "link"
+            | "meta"
+            | "source"
+            | "track"
+            | "wbr"
     )
 }
 
@@ -32,6 +43,7 @@ pub struct Template {
     sources: HashSet<String>,
     tag_stack: Vec<String>,
     parts: Vec<TemplatePart>,
+    current_route: Option<Route>,
 }
 
 enum TemplatePart {
@@ -67,15 +79,16 @@ impl Template {
         let mut template = Template {
             sources: HashSet::new(),
             tag_stack: vec![],
-            parts:  vec![TemplatePart::Content(String::new())],
+            parts: vec![TemplatePart::Content(String::new())],
+            current_route: None,
         };
-        for token in tokens{
+        for token in tokens {
             let new_parts = template.push_token(token)?;
             template.parts.extend(new_parts);
         }
         return Ok(template);
     }
-    fn push_token(&mut self, token:Token) -> Result<Vec<TemplatePart>> {
+    fn push_token(&mut self, token: Token) -> Result<Vec<TemplatePart>> {
         match token {
             Token::Doctype(_) => Ok(vec!["<DOCTYPE>".into()]),
             Token::StartTag(tag) => self.handle_start_tag(tag),
@@ -86,35 +99,64 @@ impl Template {
         }
     }
 
-    fn handle_start_tag(
-        &mut self,
-        tag: StartTag,
-    ) -> Result<Vec<TemplatePart>> {
-        let mut parts : Vec<TemplatePart>= Vec::new();
+    fn push_tag(&mut self, tag_name: &String) {
+        self.tag_stack.push(tag_name.to_string());
+    }
+    fn pop_tag(&mut self, tag_name: &String) {
+        let expected_tag = self.tag_stack.pop().expect("Tag stack is empty");
+        assert_eq!(&expected_tag, tag_name);
+    }
+    fn handle_start_tag(&mut self, tag: StartTag) -> Result<Vec<TemplatePart>> {
         let tag_name = to_utf8(tag.name)?;
+        self.push_tag(&tag_name);
+        let result: Option<Vec<TemplatePart>> = match tag_name.as_str() {
+            "x-route" => {
+                println!("REOUT {:?}", &tag.attributes);
+                None
+            }
+            "x-path" => {
+                println!("pEIANT {:?}", &tag.attributes);
+                None
+            }
+            "x-embed" => {
+                println!("EMERDB {:?}", &tag.attributes);
+                None
+            }
+            _ => Some(self.convert_tag(&tag_name, tag.attributes, tag.self_closing)?),
+        };
+        if tag.self_closing {
+            self.pop_tag(&tag_name);
+        }
+        Ok(result.unwrap_or_else(Vec::new))
+    }
+
+    fn convert_tag(
+        &mut self,
+        tag_name: &String,
+        attributes: BTreeMap<HtmlString, HtmlString>,
+        self_closing: bool,
+    ) -> Result<Vec<TemplatePart>> {
+        let mut parts: Vec<TemplatePart> = Vec::new();
         parts.push(format!("<{}", tag_name).into());
-        if tag.attributes.len() > 0 {
+        if attributes.len() > 0 {
             parts.push(" ".into());
         }
-        for (attr_name, attr_value) in tag.attributes {
+        for (attr_name, attr_value) in attributes {
             let attr_name_string = to_utf8(attr_name)?;
             parts.push(attr_name_string.into());
             if !attr_value.is_empty() {
                 parts.push(format!("=\"{}\"", to_utf8(attr_value)?).into());
             }
         }
-        if tag.self_closing {
+        if self_closing {
             parts.push("/".into());
         }
         parts.push(">".into());
         Ok(parts)
     }
 
-    fn handle_end_tag(
-        &mut self,
-        name: HtmlString,
-    ) -> Result<Vec<TemplatePart>> {
-        let mut parts : Vec<TemplatePart>= Vec::new();
+    fn handle_end_tag(&mut self, name: HtmlString) -> Result<Vec<TemplatePart>> {
+        let mut parts: Vec<TemplatePart> = Vec::new();
         let tag_name = to_utf8(name)?;
         if tag_name == "head" {
             parts.push(TemplatePart::HeadInjection);

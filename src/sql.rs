7282cde2f554b60c4759fb216db1c4b5dc27643f
wolffiex <adam@sparko.local>
2024-01-16 22:33:17 -0800

    Use channel for response

--- src/sql.rs
+++ src/sql.rs
@@ -12,17 +12,21 @@ use axum::{
     Error as AxumError, Router,
 };
 use axum_macros::debug_handler;
+use bytes::Bytes;
 use futures::stream::select;
 use futures::Stream;
 use futures::TryStreamExt;
 use futures::{stream, StreamExt};
 use serde_json::Value as Json;
-use std::collections::HashMap;
+use std::convert::Infallible;
 use std::fs;
 use std::path::{Path, PathBuf};
 use std::pin::Pin;
 use std::sync::Arc;
+use std::{collections::HashMap, time::Duration};
+use tokio::sync::mpsc::unbounded_channel;
 use tokio_postgres::{Client, NoTls};
+use tokio_stream::wrappers::UnboundedReceiverStream;
 
 pub async fn get_sql_client() -> Client {
     // Connect to the database.
@@ -52,7 +56,7 @@ pub async fn stream_sql_response(
     let sql =
         fs::read_to_string("project/src/sql/samples.sql").expect("Unable to read the SQL file");
     let sql_files_content = read_sql_files().unwrap();
-    println!("fff {:?}", sql_files_content);
+    // println!("fff {:?}", sql_files_content);
     let sources: Vec<String> = params
         .iter()
         .filter(|(key, _)| key == "source")
@@ -60,58 +64,56 @@ pub async fn stream_sql_response(
         .cloned()
         .collect();
 
+    let (tx, rx): (
+        tokio::sync::mpsc::UnboundedSender<Result<String, AxumError>>,
+        tokio::sync::mpsc::UnboundedReceiver<Result<String, AxumError>>,
+    ) = unbounded_channel();
     // Execute the query and get results.
     let sql_params: Vec<String> = vec![];
     let it = client.query_raw(&sql, sql_params).await.unwrap();
 
     // Iterate over the stream to process each row.
-    let json_stream: JsonStream = Box::pin(
-        it.map_ok(|row| {
-            let eventname = "stream1";
-            let value: Json = row.get(0);
-            // SSE
-            format!("event: {}\ndata: {}\n\n", eventname, value)
-        })
-        .map_err(AxumError::new),
-    );
+    let _ = tokio::spawn(it.for_each(move |row_result| {
+        let tx1a = tx.clone();
+        async move {
+            let event = row_result
+                .map(|row| {
+                    let eventname = "stream1";
+                    let value: Json = row.get(0);
+                    // SSE
+                    format!("event: {}\ndata: {}\n\n", eventname, value)
+                })
+                .map_err(AxumError::new);
 
-    let (client2, connection2) =
-        tokio_postgres::connect("host=haus dbname=monitoring user=adam password=adam", NoTls)
-            .await
-            .unwrap();
-
-    // The connection object performs the actual communication with the database,
-    // so spawn it off to run on its own.
-    tokio::spawn(async move {
-        if let Err(e) = connection2.await {
-            eprintln!("connection error: {}", e);
+            // tokio::time::sleep(Duration::from_secs(1)).await;
+            if let Err(_) = tx1a.send(event) {
+                // handle this error as you see fit
+                println!("WHALKJER");
+            }
         }
-    });
-    let sql2: Vec<String> = vec![];
-    let it2 = client2.query_raw(&sql, sql2).await.unwrap();
-    let js2: JsonStream = Box::pin(
-        it2.map_ok(|row| {
-            let eventname = "stream2";
-            let value: Json = row.get(0);
-            // SSE
-            format!("event: {}\ndata: {}\n\n", eventname, value)
-        })
-        .map_err(AxumError::new),
-    );
+    }));
 
-    let body = Body::from_stream(select(json_stream, js2));
+    // let merged_stream = json_stream.merge(js2);
+    // let body = Body::from_stream(merged_stream);
+    // let body = Body::from_stream(select(json_stream, js2));
 
-    // let body = Body::from_stream(json_stream);
+    // let combined_stream = futures::stream::select(json_stream, js2);
+    // let body = Body::from_stream(combined_stream);
+    let rx_stream = UnboundedReceiverStream::new(rx);
+    let body = Body::from_stream(rx_stream);
     (
         // set status code
-        StatusCode::NOT_FOUND,
+        StatusCode::OK,
+        [
+            ("Content-Type", "text/event-stream"),
+            ("x-custom", "custom"),
+        ],
         // headers with an array
-        [("x-custom", "custom")],
         body,
     )
 }
 
-type JsonStream = Pin<Box<dyn Stream<Item = Result<String, AxumError>> + Send>>;
+type JsonStream = Pin<Box<dyn Stream<Item = Result<Bytes, AxumError>> + Send>>;
 fn read_sql_files() -> Result<HashMap<String, String>> {
     let directory = Path::new("project/src/sql");
     assert!(directory.is_dir());
